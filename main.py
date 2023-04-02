0bf339440d9e92dac26ff610b136d199004be83a
Lokesh Rai <lokeshrai2019@iiitkottayam.ac.in>
2023-04-02 16:25:46 +0530

    Login/signup

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from typing import Union
 from fastapi.middleware.cors import CORSMiddleware
 import pickle
 import numpy as np
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 from pydantic import BaseModel
 import uvicorn
 
@@ -25,6 +25,10 @@ class Params(BaseModel):
     minutes: int
     latitude: float
     longitude: float
+
+class ParamsAuth(BaseModel):
+    username: str
+    password: str
 
 def DistanceToGraffiti(lat1, lon1):
     r = 6371 # Radius of earth in kilometers
@@ -54,8 +58,10 @@ with open("Notebooks/model.pkl", "rb") as file:
 with open("Notebooks/scaler.pkl", "rb") as file:
     scaler = pickle.load(file)
 
-
-
+'''
+with open("localDatabase.pkl", "wb") as file:
+    pickle.dump(dict(), file)
+'''
 
 @app.get("/")
 def read_root():
@@ -78,6 +84,41 @@ async def allDayPredictions(values: Params):
         ans.append(model.predict(test)[0][0]*100)
     return ans
 
+@app.post("/login/")
+async def login(values: ParamsAuth):
+    with open("localDatabase.pkl", "rb") as file:
+        userListing = pickle.load(file)
+    print(userListing)
+    if values.username in userListing:
+        if values.password != userListing[values.username]:
+            response = Response(content="Password Mismatch")
+            response.status_code = 401
+            return response
+        else:
+            response = Response(content="Login Success")
+            response.status_code = 200
+            return response
+    else:
+        response = Response(content="User Not Found")
+        response.status_code = 404
+        return response
+    
+@app.post("/createUser/")
+async def createUser(values: ParamsAuth):
+    with open("localDatabase.pkl", "rb") as file:
+        userListing = pickle.load(file)
+    if values.username in userListing:
+        response = Response(content="User already exists")
+        response.status_code = 403
+        return response
+    userListing[values.username] = values.password
+    with open("localDatabase.pkl", "wb") as file:
+        pickle.dump(userListing, file)
+    response = Response(content="User created successfully")
+    response.status_code = 200
+    return response
+
+        
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
 
